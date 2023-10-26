9fb5c5329ade726822a917594d0bd93bd57e62be
Mariajosepl4 <148483026+Mariajosepl4@users.noreply.github.com>
2023-10-25 22:31:22 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## changed mean to inverse
+makeCacheMatrix <- function(x = matrix(sample (1:4), 2, 2)) { ##make matrix 2 x 2 with 1-4 numbers
+  inver <- NULL
+  set <- function (y){
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setinverse <- function (inverse) inver <<- inverse
+  getinverse <- function () inver
+  list (set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
+MATRIXCOURSERA <- makeCacheMatrix(matrix (1:4, 2, 2))
 
-## Write a short comment describing this function
+> MATRIXCOURSERA$get()
+[,1] [,2]
+[1,]    1    3
+[2,]    2    4
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+> cacheSolve <- function(x, ...) {
+  +     inver <- solve()
+  +     if(!is.null(inver)) {
+    +         mesage("getting inverse matrix")
+    +         return(inver)
+    +     }
+  +     Data<-x$get()
+  +     inver<-solve(Data, ...)
+  +     x$setinver(inver)
+  +     inver
 }
+  
+  > cacheSolve(MATRIXCOURSERA)
+  Error in solve.default() : argument "a" is missing, with no default
+  
+  solve (MATRIXCOURSERA)
+  Error in solve.default(MATRIXCOURSERA) : 'a' must be a numeric matrix
+  
+  ## I GET ALL TYPES OF ERROR WHEN TRYING TO RUN THE INVERSE, I HAVE CHANGED IT FROM 1:16, 1:9, 1:4 and non of these work
