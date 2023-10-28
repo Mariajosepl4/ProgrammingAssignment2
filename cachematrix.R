e6845ee8b2893650bdfdebbd6e6cb0c97298d04f
Mariajosepl4 <148483026+Mariajosepl4@users.noreply.github.com>
2023-10-28 18:29:14 -0500

    Add files via upload
    commits to forked programming assignment Maria P

--- cachematrix.R
+++ cachematrix.R
@@ -1,46 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
-## changed mean to inverse
-makeCacheMatrix <- function(x = matrix(sample (1:4), 2, 2)) { ##make matrix 2 x 2 with 1-4 numbers
-  inver <- NULL
-  set <- function (y){
-    x <<- y
-    inver <<- NULL
+## 
+makeCacheMatrix <- function(x = matrix()) {
+  inv_matrix <- NULL  # Initiates matrix function
+  
+  set <- function(y) {
+    x <<- y  # Defines argument
+    inv_matrix <<- NULL
+    message("Getting matrix ready")
   }
-  get <- function() x
-  setinverse <- function (inverse) inver <<- inverse
-  getinverse <- function () inver
-  list (set = set, get = get,
-        setinverse = setinverse,
-        getinverse = getinverse)
+  
+#Function to get matrix
+  get <- function() {
+    message("Obtaining matrix")
+    return(x)
+  }
+
+getInverse <- function() {
+  if (!is.null(inv_matrix)) {
+    message("Obteniendo inverse matrix")
+    return(inv_matrix)
+  } else {
+    message("Inverse matrix not found")
+    return(NULL)
+  }
 }
-
-MATRIXCOURSERA <- makeCacheMatrix(matrix (1:4, 2, 2))
-
-> MATRIXCOURSERA$get()
-[,1] [,2]
-[1,]    1    3
-[2,]    2    4
 
 > cacheSolve <- function(x, ...) {
-  +     inver <- solve()
+  +     inv_matrix <- x$getInverse ()
   +     if(!is.null(inver)) {
-    +         mesage("getting inverse matrix")
-    +         return(inver)
-    +     }
-  +     Data<-x$get()
-  +     inver<-solve(Data, ...)
-  +     x$setinver(inver)
-  +     inver
+    +         message("getting inverse matrix")
+    +         return(inver) # To return inverse matrix
+    + 
+  +     data<-x$get()
+        message ("Returning inverse matrix")
+  +     inv_matrix <-solve(Data, ...)
+  +     x$getinver <- inv_matrix
+  +     return(inv_matrix)
 }
-  
-  > cacheSolve(MATRIXCOURSERA)
-  Error in solve.default() : argument "a" is missing, with no default
-  
-  solve (MATRIXCOURSERA)
-  Error in solve.default(MATRIXCOURSERA) : 'a' must be a numeric matrix
-  
-  ## I GET ALL TYPES OF ERROR WHEN TRYING TO RUN THE INVERSE, I HAVE CHANGED IT FROM 1:16, 1:9, 1:4 and non of these work
+
